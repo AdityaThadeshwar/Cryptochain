21e14de36053bf166d40c5b49135c3b227511bc2
AdityaThadeshwar <aditya.thadeshwar@gmail.com>
2021-03-28 18:10:43 +0530

    security audit fix for packages

--- average-work.js
+++ average-work.js
@@ -10,7 +10,7 @@ let prevTimestamp, nextTimestamp, nextBlock, timeDiff, average;
 
 const times = [];
 
-for (let i = 0; i < 20; i++) {
+for (let i = 0; i < 1000; i++) {
     prevTimestamp = blockchain.chain[blockchain.chain.length - 1].timestamp;
 
     blockchain.addBlock({ data: `block ${i}` });
@@ -22,5 +22,5 @@ for (let i = 0; i < 20; i++) {
 
     average = times.reduce((total, num) => (total + num)) / times.length;
 
-    console.log(`Time to mine block ${i}: ${timeDiff}ms. Difficulty: ${nextBlock.difficulty}. Average time: ${average}ms`);
+    console.log(`Time to mine block ${i+1}: ${timeDiff}ms. Difficulty: ${nextBlock.difficulty}. Average time: ${average}ms`);
 }
