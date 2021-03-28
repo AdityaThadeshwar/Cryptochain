a01c93e9d44cc29e971ce9f45537d3344cd333ed
AdityaThadeshwar <aditya.thadeshwar@gmail.com>
2021-03-28 18:45:47 +0530

    POST request configuration

--- index.js
+++ index.js
@@ -1,13 +1,26 @@
+const bodyParser = require('body-parser');
 const express = require('express');
 const Blockchain = require('./blockchain');
 
 const app = express();
 const blockchain = new Blockchain();
 
-//Endpoint for API
+app.use(bodyParser.json());
+
+//Endpoint for GET API
 app.get('/api/getBlocks', (req, res) => {
     res.json(blockchain.chain);
 });
 
+//Endpoint for POST API
+app.post('/api/mine', (req, res) => {
+
+    const { data } = req.body;
+
+    blockchain.addBlock({ data });
+
+    res.redirect('/api/getBlocks');
+});
+
 const PORT = 3000;
-app.listen(PORT, () => console.log(`Listening to port: ${PORT}`));
+app.listen(PORT, () => console.log(`listening to port: ${PORT}`));
