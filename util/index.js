f7c099ed6feef8c2e67520eff7922b5e6c9eb3ee
AdityaThadeshwar <aditya.thadeshwar@gmail.com>
2021-03-29 16:45:53 +0530

    generate public key, sign data and verify the transaction

--- util/index.js
+++ util/index.js
@@ -1,6 +1,19 @@
 const EC = require('elliptic').ec;
+const cryptoHash = require('./crypto-hash');
 
 //Elliptic cryptography using secp256k1 implementation (Same used by BTC)
 const ec = new EC('secp256k1');
 
-module.exports = { ec };
+//verify signature of a transaction
+const verifySignature = ({ publicKey, data, signature }) => {
+
+    //verify method only available withn an instance of key object
+    //so create temp key using public key
+    const keyFromPublic = ec.keyFromPublic(publicKey, 'hex');
+
+    //verify() requires data in hex format
+    return keyFromPublic.verify(cryptoHash(data), signature);
+
+}
+
+module.exports = { ec, verifySignature };
--- wallet/index.js
+++ wallet/index.js
@@ -1,16 +1,23 @@
 const { STARTING_BALANCE } = require('../config');
 const { ec } = require('../util');
+const cryptoHash = require('../util/crypto-hash');
 
 class Wallet{
     constructor(){
         this.balance = STARTING_BALANCE;
 
-        const keyPair = ec.genKeyPair();
+        //generate public key for the wallet
+        this.keyPair = ec.genKeyPair();
+        this.publicKey = this.keyPair.getPublic().encode('hex');
+    }
 
-        this.publicKey = keyPair.getPublic().encode('hex');
+    //sign the data using sign() method provided by elliptic library
+    //sign() requires data in hex format 
+    sign(data) {
+        return this.keyPair.sign(cryptoHash(data));
     }
 }
 
-const wallet = new Wallet();
+//const wallet = new Wallet();
 
 module.exports = Wallet;
